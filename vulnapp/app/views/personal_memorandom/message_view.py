169272b55578e15dd17298c1299179f7df9e34c5
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2021-04-11 21:24:58 +0900

    SQLインジェクションを追加

--- vulnapp/app/views/personal_memorandom/message_view.py
+++ vulnapp/app/views/personal_memorandom/message_view.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
+from django.db import connection
+
 from app.models import Message
 
 
@@ -12,6 +14,14 @@ class MessageView(LoginRequiredMixin, View):
 
         mid = request.GET.get('message_id')
         if mid:
+            with connection.cursor() as cursor:
+                sql ="""
+                    SELECT "messages"."id", "messages"."body", "messages"."created_at", "messages"."updated_at", "messages"."user_id" FROM "messages" WHERE "messages"."id" = {};
+                """.format(mid,mid)
+                # print(sql)
+                c = cursor.executescript(sql)
+                result = c.fetchall()
+                # print(result)
             return redirect(to='search_message', message_id=mid)
 
         if message_id is not None:
@@ -19,7 +29,6 @@ class MessageView(LoginRequiredMixin, View):
             context['message'] = message
 
             return render(request, 'messages/search_message.html', context=context)
-
 
         return render(request, 'messages/search_message.html', context=context)
 
