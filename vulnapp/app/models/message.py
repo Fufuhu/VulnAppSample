601506193324af7d02d031fa4b5185d432322255
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2021-04-11 14:41:19 +0900

    メッセージを投稿できるようにした

--- vulnapp/app/models/message.py
+++ vulnapp/app/models/message.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib import admin
 
 
 class Message(models.Model):
@@ -9,5 +10,10 @@ class Message(models.Model):
 
     body = models.TextField(verbose_name='本文')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
-    updated_ad = models.DateTimeField(verbose_name='更新日時', auto_now=True)
-    user = models.ForeignKey(User)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('body', 'created_at', 'created_at', 'updated_at')
--- vulnapp/app/views/personal_memorandom/message_view.py
+++ vulnapp/app/views/personal_memorandom/message_view.py
@@ -1,10 +1,25 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from app.models.message import Message
 
-class MessageView(View):
+
+class MessageView(LoginRequiredMixin, View):
 
     def get(self, request):
         context = {}
 
         return render(request, 'messages/post_message.html', context=context)
+
+    def post(self, request):
+        context={}
+
+        user = request.user
+        body = request.POST.get('body')
+        print(body)
+
+        message = Message(body=body, user=user)
+        message.save()
+
+        return render(request, 'messages/list_messages.html', context=context)
