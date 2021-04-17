ef480975b413e83be3ae197c8a6f5514075fd3a3
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2021-04-17 15:04:55 +0900

    faeture: メッセージの削除機能を追加

--- vulnapp/app/urls.py
+++ vulnapp/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
 from .views.home_view import HomeView
+from .views.personal_memorandom.delete_message_view import DeleteMessageView
 from .views.personal_memorandom.message_view import MessageView
 from .views.personal_memorandom.messages_view import MessagesView
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('message/', MessageView.as_view(), name='message'),
     path('message/<int:message_id>/', MessageView.as_view(), name='search_message'),
+    path('message/<int:message_id>/delete', DeleteMessageView.as_view(), name='delete_message'),
     path('messages/', MessagesView.as_view(), name='messages'),
 ]
